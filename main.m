70df14ca12d76e9933640423243c71dc19af16ce
ThangSparrow <30438243+ThangSparrow@users.noreply.github.com>
2020-12-23 20:37:22 +0100

    [Consistent]
    - Fixing animation scripts
    - Adding position tuned controller (for only z)

--- main.m
+++ main.m
@@ -10,9 +10,9 @@ drone1 = Drone(drone1_params, drone1_initStates, drone1_Inputs, drone1_gains);
 simTime = 1;
 
 %% RUN SIMULATION
-commandSig(1) = deg2rad(10);
-commandSig(2) = deg2rad(10);
-commandSig(3) = deg2rad(30);
+commandSig(1) = 0.0;
+commandSig(2) = 0.0;
+commandSig(3) = -3.0;
 commandSig(4) = 0.0;
 Animation;
                     
